a0ba606886c3c2b1b27f28249088ed6b172dc6a8
Tale <giangnguyen.tale@gmail.com>
2018-09-06 23:55:01 +0700

    added TikiDi to handle global scope

---
+++ di/di/src/main/java/vn/tiki/android/di/TikiDi.kt
@@ -0,0 +1,35 @@
+package vn.tiki.android.di
+
+@Suppress("MemberVisibilityCanBePrivate")
+
+object TikiDi {
+
+  @PublishedApi
+  internal val objectGraphs = mutableListOf<ObjectGraph>()
+
+  fun openScope(vararg modules: Module) {
+    require(modules.isNotEmpty()) { "modules must not be empty" }
+
+    val objectGraph: ObjectGraph = if (objectGraphs.isEmpty()) {
+      ObjectGraph.create(modules.asList())
+    } else {
+      objectGraphs.last().plus(modules.asList())
+    }
+
+    objectGraphs.add(objectGraph)
+  }
+
+  fun closeScope() {
+    if (objectGraphs.isEmpty()) {
+      return
+    }
+    objectGraphs.removeAt(objectGraphs.size - 1)
+  }
+
+  inline fun <reified T> inject(): T {
+    val objectGraph = objectGraphs.lastOrNull()
+      ?: throw IllegalStateException("no scope found. You must open scope first")
+
+    return objectGraph[T::class.java]
+  }
+}
